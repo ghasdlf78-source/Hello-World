937512d0c7f48ee2bd7d24ff935e43b6a83cb196
Duong <ghasdlf78@gmail.com>
2025-11-29 21:39:19 +0700

    update new.java

--- home/src/New.java
+++ home/src/New.java
@@ -1,2 +1,9 @@
-public class New {
+public static void main(String[] args) {
+    for (int i = 0; i <= 99; i++) {
+        if (i % 2 == 0) {
+            System.out.println(i + " :even");
+        } else {
+            System.out.println(i + " :odd");
+        }
+    }
 }
